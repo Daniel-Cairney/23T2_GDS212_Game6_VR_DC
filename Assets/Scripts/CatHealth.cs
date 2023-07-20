7f83d0f08f87a85df67ca704a4f3c6a5601beb55
Daniel-Cairney lab101 <114543733+Daniel-Cairney@users.noreply.github.com>
2023-07-20 16:04:06 +1000

    Added cat hp bar, new enemies, better graphics, and sfx

--- Assets/Scripts/CatHealth.cs
+++ Assets/Scripts/CatHealth.cs
@@ -1,14 +1,18 @@
 using UnityEngine;
+using UnityEngine.UI;
 namespace DanielCairney
 {
     public class CatHealth : MonoBehaviour
     {
         public int maxHealth = 100;
         public int currentHealth;
-
+        public Slider healthBar;
+        public AudioSource catHitSound;
+        
         private void Start()
         {
             currentHealth = maxHealth;
+            UpdateHealthBar();
         }
 
         private void OnTriggerEnter(Collider other)
@@ -23,12 +27,27 @@ namespace DanielCairney
         private void TakeDamage(int damage)
         {
             currentHealth -= damage;
-
+            catHitSound.Play();
             if (currentHealth <= 0)
             {
                 Die();
             }
+
+            UpdateHealthBar();
         }
+
+        private void UpdateHealthBar()
+        {
+            Debug.Log("YUIPPPYRPT");
+            // Calculate the fill amount of the health bar image based on health changes
+            float fillAmount = (float)currentHealth / maxHealth;
+            Debug.Log("Decrease Fill");
+            // Update the health bar image's fill amount
+            healthBar.value = fillAmount;
+            Debug.Log("2nd Decrease Fill");
+        }
+
+
 
         private void Die()
         {
